8e7227fc249840da58e8d0f335a0e0194bb7beca
virus4297 <nammanewsvirus4297@users.noreply.github.com>
2020-04-09 04:37:25 +0530

    Static data works

--- app/src/main/java/com/example/wifiuncovered/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/wifiuncovered/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.wifiuncovered.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,25 +12,42 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.wifiuncovered.MainActivity;
+import com.example.wifiuncovered.MyAdapter;
 import com.example.wifiuncovered.R;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+        HomeViewModel homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        final TextView textView = root.findViewById(R.id.textView3);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+
+        RecyclerView recyclerView = root.findViewById(R.id.ip_RecyclerView);
+        //initRecyclerView
+        recyclerView.setHasFixedSize(true);
+        //get adapter data from HomeViewModel
+        MyAdapter adapter=homeViewModel.getAdapter(getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+
         return root;
     }
+
+
+
 }
--- app/src/main/java/com/example/wifiuncovered/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/wifiuncovered/ui/home/HomeViewModel.java
@@ -1,5 +1,12 @@
 package com.example.wifiuncovered.ui.home;
 
+import android.content.Context;
+
+import com.example.wifiuncovered.MyAdapter;
+
+import java.util.ArrayList;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -7,13 +14,36 @@ import androidx.lifecycle.ViewModel;
 public class HomeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    public int myDatasetcount;
+    public ArrayList<String> images_ArrayList = new ArrayList<>();
+    public ArrayList<String> status_ArrayList = new ArrayList<>();
+    public ArrayList<String> ip_ArrayList = new ArrayList<>();
+    public ArrayList<String> desc_ArrayList = new ArrayList<>();
+    public String ip,host,ipAddress;
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
+
+        ip_ArrayList.add("192.168.1.1");
+        status_ArrayList.add("UP");
+        images_ArrayList.add("https://cdn.pixabay.com/photo/2020/03/19/04/58/coconut-trees-4946270_960_720.jpg");
+        desc_ArrayList.add("hehe");
+        myDatasetcount++;
+        ip_ArrayList.add("192.168.1.1");
+        status_ArrayList.add("UP");
+        images_ArrayList.add("https://cdn.pixabay.com/photo/2020/03/19/04/58/coconut-trees-4946270_960_720.jpg");
+        desc_ArrayList.add("hehe");
+        myDatasetcount++;
+
     }
 
     public LiveData<String> getText() {
         return mText;
     }
+    public MyAdapter getAdapter(Context context){
+        MyAdapter adapter = new MyAdapter(context,ip_ArrayList, desc_ArrayList,status_ArrayList, images_ArrayList,myDatasetcount);
+        return adapter;
+    }
+
 }
